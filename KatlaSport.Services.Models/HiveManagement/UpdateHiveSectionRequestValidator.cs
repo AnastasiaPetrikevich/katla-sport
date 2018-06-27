a921a0d744cc43d1b7cee4ce39b5abc7706e678e
Anastasiya Petrykevich <nastya.petrikevich@gmail.com>
2018-06-28 00:35:58 +0300

    Merge step13. Added methods for adding, editing, and deleting the HiveSection entity in the HiveSectionsController controller and in the HiveSectionService service.

---
+++ KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace KatlaSport.Services.HiveManagement
+{
+    /// <summary>
+    /// Represents a validator for <see cref="UpdateHiveSectionRequestValidator"/>
+    /// </summary>
+    public class UpdateHiveSectionRequestValidator : AbstractValidator<UpdateHiveSectionRequest>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdateHiveSectionRequestValidator"/> class.
+        /// </summary>
+        public UpdateHiveSectionRequestValidator()
+        {
+            RuleFor(r => r.Name).Length(4, 60);
+            RuleFor(r => r.Code).Length(5);
+        }
+    }
+}
